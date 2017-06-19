ef8dea0adfa93f91e0b29e94a96b4068802b2dde
showkw <showkw@163.com>
2017-06-19 23:07:33 +0800

    'showkw-后台商品列表'

---
+++ app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * File Name: ProductController.php
+ * Description: 商品管理控制器
+ * Created by PhpStorm.
+ * Group: FiresGroup
+ * Auth: Showkw
+ * Date: 2017/6/19
+ * Time: 11:34
+ */
+
+
+namespace App\Http\Controllers\Admin;
+
+use App\Entity\Product;
+use App\Http\Requests\Admin\ProductRequest;
+use App\Http\Controllers\Controller;
+use DB;
+
+class ProductController extends Controller
+{
+    /**
+     * 商品列表页
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $productList = Product::all();
+        $productList = Product::paginate(5);
+
+//        dd($productList);
+        return view('admin.product.index',compact('productList', 'total'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProductRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProductRequest $request,$id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProductRequest $request,$id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ProductRequest $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductRequest $request,$id)
+    {
+        return DB::table('product')->delete($id);
+    }
+}
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,19 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
+$factory->define(App\Entity\Product::class, function (Faker\Generator $faker) {
+//    return [
+//        'name' => $faker->name,
+//        'email' => $faker->safeEmail,
+//        'password' => bcrypt(str_random(10)),
+//        'remember_token' => str_random(10),
+//    ];
+
+    return[
+        'cid'=> $faker->randomDigitNotNull,
+        'bid'=> $faker->randomDigitNotNull,
+        'p_name'=> $faker->sentence,
+        'price' => $faker->randomFloat(),
+        'market_price' => $faker->randomFloat(),
     ];
 });
