3da4c34a951232ca5f55e7ae10a7abdcff268037
showkw <showkw@163.com>
2017-07-05 09:18:39 +0800

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 
 /*
 |--------------------------------------------------------------------------
@@ -10,23 +10,19 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-////肖开文-路由文件
-// include('Route/showkw.php');
-//
-////王明-路由文件
-// //include('Route/wim.php');
-//
-////龙彪-路由文件
+//肖开文-路由文件
+ include('Route/showkw.php');
+
+//王明-路由文件
+ //include('Route/wim.php');
+
+//龙彪-路由文件
 // include('Route/long.php');
-//
-////潘珺-路由文件
+
+//潘珺-路由文件
 //include('Route/jun.php');
-//
-////这个不要动
-//Route::get('/admin/welcome', function(){
-//    return view('welcome');
-//});
-//
-//Route::get('admin', function () {
-//    return view('login');
-//});
+
+//这个不要动
+Route::get('/admin/welcome', function(){
+    return view('welcome');
+});
