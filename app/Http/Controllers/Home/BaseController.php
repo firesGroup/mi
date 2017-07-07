f9bc8fa8da27d4da7f0f04abb6b52b2fc1299e51
showkw <showkw@163.com>
2017-07-07 10:01:43 +0800

    Update BaseController.php

--- app/Http/Controllers/Home/BaseController.php
+++ app/Http/Controllers/Home/BaseController.php
@@ -13,13 +13,14 @@ class BaseController extends Controller
 
     public function __construct()
     {
+        //头部横排导航 商品
         $header_nav = HomeC::headerNav();
+        //头部纵向导航 商品
+        $header_nav_port = HomeC::headerNavPort();
 
-//        ;
         view()->share([
-
-            'header_nav' => $header_nav,
-
+            'header_nav'=>$header_nav,
+            'header_nav_port'=>$header_nav_port,
         ]);
     }
 }
