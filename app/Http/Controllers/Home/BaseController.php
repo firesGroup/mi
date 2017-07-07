54941d7d69aaeb8c22d7a96efac9d44e2a799be5
Wim041886 <463803696@qq.com>
2017-07-07 10:29:40 +0800

    合并

--- app/Http/Controllers/Home/BaseController.php
+++ app/Http/Controllers/Home/BaseController.php
@@ -1,25 +1,20 @@
 <?php
-
 namespace App\Http\Controllers\Home;
-
-
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Home\HomeController as HomeC;
 use Session;
 use DB;
-
 class BaseController extends Controller
 {
-
     public function __construct()
     {
+        //头部横排导航 商品
         $header_nav = HomeC::headerNav();
-
-//        ;
+        //头部纵向导航 商品
+        $header_nav_port = HomeC::headerNavPort();
         view()->share([
-
-            'header_nav' => $header_nav,
-
+            'header_nav'=>$header_nav,
+            'header_nav_port'=>$header_nav_port,
         ]);
     }
 }
